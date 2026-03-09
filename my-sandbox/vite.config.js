6fb0c47e7437892750c40635ee79e721e5748ec0
devkavach22 <developer@kavachglobal.com>
2026-03-09 15:58:04 +0530

    new chnages doen

--- my-sandbox/vite.config.js
+++ my-sandbox/vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    port: 6000,
-    host: true,          // allows access from LAN / 0.0.0.0
-    strictPort: true,    // fail if port 6000 is in use
-    cors: true           // allow all origins (*)
-  },
-
   plugins: [
     tailwindcss(),
   ],
