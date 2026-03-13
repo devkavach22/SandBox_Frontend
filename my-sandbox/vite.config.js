19a23c50e7ddede5f217db9f7c88850fba64b23d
devkavach22 <developer@kavachglobal.com>
2026-03-13 14:46:08 +0530

    added the domin

--- my-sandbox/vite.config.js
+++ my-sandbox/vite.config.js
@@ -5,4 +5,23 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+
+  server: {
+    host: true, // Allow external access
+
+    // All domains you listed
+    allowedHosts: [
+      'sandbox.kavachglobal.com',
+      '110.226.96.148'
+    ],
+
+    // Useful for HMR when accessed through proxy/other domain
+    hmr: {
+      host: 'sandbox.kavachglobal.com',
+      protocol: 'https'
+    },
+
+    // Optional: set main origin for generated asset URLs
+    origin: 'https://sandbox.kavachglobal.com:5173'
+  }
 })
